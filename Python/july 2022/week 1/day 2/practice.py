ab9681a5a88ec40f84b9e4e6d44b6cfd4fd63143
Vaishnav13 <108188697+Vaishnav13@users.noreply.github.com>
2022-07-14 13:12:02 +0530

    Add files via upload

---
+++ Python/july 2022/week 1/day 2/practice.py
@@ -0,0 +1,17 @@
+#Type Conversion. 
+#Convert from one type to another type
+x = 1
+y = 2.4
+z = 5j
+
+a = float(x)  
+b = int(y)
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
